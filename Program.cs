8f72507bbe37ec822bc8b1bc25533c79b2155b52
SandERFills <salagaev98@mail.ru>
2021-02-07 21:52:09 +0300

    solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonachi
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input quantity steps");
+            byte stepsQuantity = byte.Parse(Console.ReadLine());
+            int fist = 0, second = 1, result=0,n=0;
+            while (n < 10)
+            {
+                result = fist + second;
+                fist = second;
+                second = result;
+                n++;
+                Console.WriteLine(result);
+            }
+
+
+        }
+    }
+}
